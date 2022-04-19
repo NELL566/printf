e3e6054901894ce5b576a7bcec414fdbe69a90bf
NELL566 <odemeroheaven@gmail.com>
2022-04-20 00:01:21 +0100

    Task 0

--- printf.c
+++ printf.c
@@ -1,45 +1,51 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
-/**
- * @brief
- *
- */
 
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
+ */
 int _printf(const char *format, ...)
 {
-	int i;
-	int len = 0;
-	char *newstr = NULL;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	while (format[len] != '\0')
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		len++;
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-
-	newstr = malloc(sizeof(char) * (len + 1));
-
-	if (newstr == NULL)
-	{
-		return (1);
-	}
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		newstr[i] = format[i];
-	}
-	newstr[i] = '\0';
-
-	write(1, format, len);
-	free(newstr);
-	return (0);
-}
-
-int main(void)
-{
-	char *s = "Let's try to printf a simple sentence.\n";
-	_printf(s);
-	printf("%s", s);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
